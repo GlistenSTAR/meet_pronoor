3bcd71d1010b3d1e1a15a8bdc66ad09758745477
NightFury0109 <nightfury0109@gmail.com>
2021-11-16 01:12:14 +0100

    Improve UI

---
+++ models/friends.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = require('../utils/database');
+
+const Friend = sequelize.define('friend', {
+  id: {
+    type: Sequelize.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  user: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  nickname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  avatar: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  time: {
+    type: Sequelize.DATE,
+    allowNull: false
+  }
+});
+
+module.exports = Friend;
